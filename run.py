fadd7e58476e3505f213698e95736d346fc37921
Misha-Mayskiy <Shofer2007@gmail.com>
2025-05-31 13:21:44 +0300

    feat: deploy files added

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
+import os
+
+from flask import current_app
+from flask_migrate import upgrade
+
 from app import create_app, db
 from app.models import User, Game
 
 app = create_app()
+
+if os.environ.get('RENDER') == 'true' or os.environ.get('FLASK_ENV') == 'production':
+    with app.app_context():
+        try:
+            current_app.logger.info("Попытка применить миграции БД...")
+            upgrade()
+            current_app.logger.info("Миграции БД успешно применены (или уже были актуальны).")
+        except Exception as e:
+            current_app.logger.error(f"Ошибка при автоматическом применении миграций: {e}")
 
 
 @app.shell_context_processor
@@ -10,4 +24,4 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
