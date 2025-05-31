da80b6a2dddced5b6712e2030be33e5a52d3dfc5
Misha-Mayskiy <Shofer2007@gmail.com>
2025-05-31 13:00:38 +0300

    feat: deploy files added

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    DEFAULT_SQLITE_URL = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or DEFAULT_SQLITE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     OPENID_FS_STORE_PATH = os.path.join(basedir, 'openid_store')
     STEAM_API_KEY = os.environ.get('STEAM_API_KEY')
